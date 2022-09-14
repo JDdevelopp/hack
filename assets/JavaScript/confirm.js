5b72feba93d32b37b33c5e34a6f74e5b4ab02c94
JDdevelopp <jagratadeb2007@gmail.com>
2022-09-14 20:36:10 +0530

    Update confirm.js

--- assets/JavaScript/confirm.js
+++ assets/JavaScript/confirm.js
@@ -1,5 +1,5 @@
 if (confirm("Are you sure that you want to be hacked? 👿") == true) {
-    alert("That button 'OK' gave me the access to your device.")
+    alert("That button 'OK' gave me the access to your device. Now I will do what ever I wish to do👿.")
 } else {
     window.close()
 }
